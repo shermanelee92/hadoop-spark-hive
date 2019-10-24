bc2934853a388950662ff5d88ca98e5593766315
Shermane Lee <shermaneleeqianhui@gmail.com>
2019-10-24 18:02:16 +0800

    Fix database connection problem, add start up script

--- master/setup-ssh.sh
+++ master/setup-ssh.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 echo "creating ssh key for hadoop-master"
-docker exec -it hadoop-master ssh-keygen -t rsa -b 4096
-echo "moving ssh key for hadoop-master"
-docker exec hadoop-master bash -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
+docker exec -it hadoop-master ssh-keygen -t rsa -b 4096 -f /home/hadoop/.ssh/id_rsa
 echo "change owner for hadoop-master"
+docker exec hadoop-master bash -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
 docker exec hadoop-master bash -c 'chmod og-wx ~/.ssh/authorized_keys'
 docker exec -it hadoop-master bash -c "su root -c 'service ssh restart'"
-docker exec hadoop-master bash -c 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost'
-
+# docker exec hadoop-master bash -c 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost'
 docker exec -it hadoop-slave1 bash -c "su root -c 'service ssh restart'"
-docker exec -it hadoop-master bash -c 'ssh-copy-id -i ~/.ssh/id_rsa.pub root@hadoop-slave1'
 docker exec -it hadoop-slave2 bash -c "su root -c 'service ssh restart'"
-docker exec -it hadoop-master bash -c 'ssh-copy-id -i ~/.ssh/id_rsa.pub root@hadoop-slave2'
+docker exec -it hadoop-master bash -c "ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@hadoop-slave1"
+docker exec -it hadoop-master bash -c "ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@hadoop-slave2"
+docker exec -it hadoop-slave1 bash -c "su root -c 'service ssh restart'"
+docker exec -it hadoop-slave2 bash -c "su root -c 'service ssh restart'"
+docker exec -it hive-db bash -c "su root -c 'service ssh restart'"
 
+
+
+# echo "creating ssh key for hadoop-master"
+# docker exec -it hadoop-master ssh-keygen -t rsa -b 4096
+# echo "moving ssh key for hadoop-master"
+# docker exec hadoop-master bash -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
+# echo "change owner for hadoop-master"
+# docker exec hadoop-master bash -c 'chmod og-wx ~/.ssh/authorized_keys'
+# docker exec -it hadoop-master bash -c "su root -c 'service ssh restart'"
+# # docker exec hadoop-master bash -c 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost'
+# docker exec -it hadoop-slave1 bash -c "su root -c 'service ssh restart'"
+# docker exec -it hadoop-slave2 bash -c "su root -c 'service ssh restart'"
+# docker exec -it hadoop-master bash -c "ssh-copy-id -i ~/.ssh/id_rsa.pub root@hadoop-slave1"
+# docker exec -it hadoop-master bash -c "ssh-copy-id -i ~/.ssh/id_rsa.pub root@hadoop-slave2"
+# docker exec -it hadoop-slave1 bash -c "su root -c 'service ssh restart'"
+# docker exec -it hadoop-slave2 bash -c "su root -c 'service ssh restart'"
+
+docker exec -it hadoop-master bash -c "su root -c 'service ssh restart'"
 docker exec hadoop-master /usr/local/hadoop/bin/hdfs namenode -format
 docker exec -it hadoop-master /usr/local/hadoop/sbin/start-all.sh
 
--- start-containers.sh
+++ start-containers.sh
@@ -1,11 +1,13 @@
 PWD=`pwd`
 HOST_MASTER_HADOOP_CONF_PATH="$PWD/master/hadoop/conf"
+HOST_MASTER_SPARK_CONF_PATH="$PWD/master/spark/conf"
 CONT_MASTER_HADOOP_CONF_PATH='/usr/local/hadoop/etc/hadoop'
 HOST_HIVE_CONF_PATH="$PWD/database/hive/conf"
+DAGS_FOLDER="$PWD/dags"
 
 docker run -itd -v $HOST_MASTER_HADOOP_CONF_PATH:$CONT_MASTER_HADOOP_CONF_PATH \
--v $HOST_MASTER_HADOOP_CONF_PATH:/usr/local/spark/conf \
--v dags:/usr/local/dags \
+-v $HOST_MASTER_SPARK_CONF_PATH:/usr/local/spark/conf \
+-v $DAGS_FOLDER:/usr/local/dags \
 -p 8088:8088 -p 50070:50070 -p 9001:9001 -p 50010:50010 -p 4040:4040 -p 8081:8081 \
 --network=my-bridge-network \
 --name=hadoop-master \
