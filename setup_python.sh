2651b5a50fa567e6e2713ac2dbfd6303d4bb812b
wp <deadstroke@gmail.com>
2019-12-14 11:34:30 +0800

    feat: finnet Py3 environment setup

--- setup_python.sh
+++ setup_python.sh
@@ -1,11 +1,12 @@
+# create finnet Python 2 environment
 docker exec -it hadoop-master python2 get-pip.py
-docker exec -it hadoop-master python2 -m pip install ipykernel
-
-# more convenient to put requirements.txt in dags folder lol
 docker exec -it hadoop-master python2 -m pip install -r dags/requirements.txt
-
-# installing ipykernel automatically installs a wrong "python2" kernel
-docker exec -it hadoop-master jupyter kernelspec uninstall python2
-
 docker exec -it --user hadoop hadoop-master python2 -m ipykernel install --user --name finnet-pipeline --display-name "finnet-pipeline (Python 2)"
+
+docker exec -it hadoop-master python3 -m pip install -r dags/requirements_py3.txt
+docker exec -it --user hadoop hadoop-master python3 -m ipykernel install --user --name finnet-pipeline_py3 --display-name "finnet-pipeline (Python 3)"
+
+# installing ipykernel installs a wrong "python2" kernel
+docker exec -it hadoop-master jupyter kernelspec uninstall python2
+docker exec -it hadoop-master jupyter kernelspec uninstall python3
 
